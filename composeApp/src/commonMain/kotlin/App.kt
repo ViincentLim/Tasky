db1094b9b314e68c07be0b51d284a4d77983de74
vincentlim <vincentlimjingen@gmail.com>
2024-07-05 22:20:50 +0800

    refactor(tasks): refactor App and CreateTaskRow to move taskName into CreateTaskRow
    - move safeDrawingPadding from Box to Column to remove unnecessary Box
    - move taskName into CreateTaskRow to delegate handling of taskName state to CreateTaskRow
    - abstract onCreateTask event to parent of CreateTaskRow

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,3 @@
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -7,16 +6,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import database.AppDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
-import model.Task
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import ui.CreateTaskRow
 import ui.TaskItemView
@@ -28,38 +24,22 @@ fun App(database: AppDatabase) {
     val tasks by remember { tasksDao.getAll() }.collectAsState(emptyList())
     val coroutineScopeIO = CoroutineScope(Dispatchers.IO)
 
-    var createTaskName by remember { mutableStateOf("") }
-    val onCreateTask = onCreateTask@{
-        if (createTaskName.trim().isBlank()) return@onCreateTask
-        coroutineScopeIO.launch {
-            tasksDao.insert(
-                Task(
-                    name = createTaskName.trim()
-                )
-            )
-            createTaskName = ""
-        }
-        return@onCreateTask
-    }
-
     MaterialTheme {
-        Box(Modifier.safeDrawingPadding()) {
-            Column {
-                LazyColumn(Modifier.weight(1f)) {
-                    items(items = tasks, key = { it.id }) { task ->
-                        TaskItemView(task = task, onDelete = {
-                            coroutineScopeIO.launch {
-                                tasksDao.delete(task)
-                            }
-                        })
-                    }
+        Column(Modifier.safeDrawingPadding()) {
+            LazyColumn(Modifier.weight(1f)) {
+                items(items = tasks, key = { it.id }) { task ->
+                    TaskItemView(task = task, onDelete = {
+                        coroutineScopeIO.launch {
+                            tasksDao.delete(task)
+                        }
+                    })
                 }
-                CreateTaskRow(
-                    taskName = createTaskName,
-                    onTaskNameChange = { createTaskName = it },
-                    onCreateTask = onCreateTask,
-                )
             }
+            CreateTaskRow(onCreateTask = { task ->
+                coroutineScopeIO.launch {
+                    tasksDao.insert(task)
+                }
+            })
         }
     }
 }
--- composeApp/src/commonMain/kotlin/ui/CreateTaskRow.kt
+++ composeApp/src/commonMain/kotlin/ui/CreateTaskRow.kt
@@ -2,6 +2,7 @@ package ui
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
@@ -13,28 +14,41 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import model.Task
 
 @Composable
 fun CreateTaskRow(
-    taskName: String,
-    onTaskNameChange: (String) -> Unit, onCreateTask: () -> Unit,
+    onCreateTask: (Task) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var taskName by remember { mutableStateOf("") }
+
+    fun createTask() {
+        onCreateTask(Task(name = taskName.trim()))
+        taskName = ""
+    }
+
     Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
         TaskNameTextField(
             taskName = taskName,
-            onTaskNameChange = onTaskNameChange, onCreateTask = onCreateTask,
+            onTaskNameChange = { taskName = it },
+            onDone = {
+                createTask()
+            },
             modifier = Modifier.weight(1f)
         )
         CreateTaskButton(
-            onClick = onCreateTask,
-            modifier = Modifier.padding(8.dp)
+            onClick = { createTask() }, modifier = Modifier.padding(8.dp)
         )
     }
 }
@@ -43,32 +57,29 @@ fun CreateTaskRow(
 private fun TaskNameTextField(
     taskName: String,
     onTaskNameChange: (String) -> Unit,
-    onCreateTask: () -> Unit,
+    onDone: KeyboardActionScope.() -> Unit,
     modifier: Modifier = Modifier
 ) {
     OutlinedTextField(
         value = taskName,
         onValueChange = onTaskNameChange,
         placeholder = { Text("Enter a new task", fontSize = 20.sp) },
-        keyboardActions = KeyboardActions(onDone = { onCreateTask() }),
+        keyboardActions = KeyboardActions(onDone = onDone),
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
         modifier = modifier,
         textStyle = MaterialTheme.typography.body1.merge(fontSize = 20.sp),
         colors = TextFieldDefaults.outlinedTextFieldColors(
-            unfocusedBorderColor = Color.Transparent,
-            focusedBorderColor = Color.Transparent
+            unfocusedBorderColor = Color.Transparent, focusedBorderColor = Color.Transparent
         ),
     )
 }
 
 @Composable
 private fun CreateTaskButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    onClick: () -> Unit, modifier: Modifier = Modifier
 ) {
     IconButton(
-        onClick = onClick,
-        modifier = modifier
+        onClick = onClick, modifier = modifier
     ) {
         Icon(Icons.Default.Add, contentDescription = "Create task")
     }
